eaea021bc8e91d069c7b8edfe7dbf4060d1283c9
Maximilian Krüger <kruemaxi@googlemail.com>
2014-10-11 17:25:04 +0200

    working example of startup and shutdown

--- example.js
+++ example.js
@@ -1,30 +1,148 @@
-var vorrang = require('./src/vorrang');
+var Promise = require('bluebird');
 
-var dag = new vorrang.Dag()
-  .before('tytos', 'tywin')
+var Vorrang = require('./src/vorrang');
 
-  .before('tywin', 'jaime')
-  .before('joanna', 'jaime')
+///////////////////////////////////////////////////////////////////////////////////
+// services
 
-  .before('tywin', 'cersei')
-  .before('joanna', 'cersei')
+var services = {
+  redisOne: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  redisTwo: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  serverOne: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  serverTwo: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  serverThree: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  elasticSearch: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  mailAPI:{
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  cache: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  postgres: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  loadBalancer: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  workerOne: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  },
+  workerTwo: {
+    start: function() { return Promise.delay(100); },
+    stop: function() { return Promise.delay(100); }
+  }
+}
 
-  .before('tywin', 'tyrion')
-  .before('joanna', 'tyrion')
+///////////////////////////////////////////////////////////////////////////////////
+// order
 
-  .before('jaime', 'tommen')
-  .before('cersei', 'tommen')
+var dag = new Vorrang()
+  .before('redisOne', 'cache')
+  .before('redisTwo', 'cache')
 
-  .before('jaime', 'jeoffrey')
-  .before('cersei', 'jeoffrey')
+  .before('cache', 'serverOne')
+  .before('cache', 'serverTwo')
+  .before('cache', 'serverThree')
 
-  .before('jaime', 'mycella')
-  .before('cersei', 'mycella')
+  .before('postgres', 'serverOne')
+  .before('postgres', 'serverTwo')
+  .before('postgres', 'serverThree')
+
+  .before('elasticSearch', 'serverOne')
+  .before('elasticSearch', 'serverTwo')
+  .before('elasticSearch', 'serverThree')
+
+  .before('elasticSearch', 'workerOne')
+  .before('elasticSearch', 'workerTwo')
+
+  .before('postgres', 'workerOne')
+  .before('postgres', 'workerTwo')
+
+  .before('mailAPI', 'workerOne')
+  .before('mailAPI', 'workerTwo')
+
+  .before('serverOne', 'loadBalancer')
+  .before('serverTwo', 'loadBalancer')
+  .before('serverThree', 'loadBalancer')
   ;
 
-console.log(dag);
+///////////////////////////////////////////////////////////////////////////////////
+// start & stop
 
-console.log('jaime < jeoffrey', dag.isBefore('jaime', 'jeoffrey'));
+var starting = [];
+var running = [];
+var stopping = [];
+var stopped = [];
 
-console.log('minElements', dag.minElements());
-console.log('maxElements', dag.maxElements());
+var start = function(names) {
+  return Promise.all(names.map(function(name) {
+    if (-1 !== starting.indexOf(name)) {
+      return;
+    }
+    if (-1 !== running.indexOf(name)) {
+      return;
+    }
+    console.log('starting: ' + name);
+    var callback = services[name].start;
+    var promise = Promise.resolve(callback());
+    starting.push(name);
+    return promise.then(function() {
+      console.log('running:  ' + name);
+      running.push(name);
+      return start(dag.whereAllParentsIn(running));
+    });
+  }));
+};
+
+var stop = function(names) {
+  return Promise.all(names.map(function(name) {
+    if (-1 !== stopping.indexOf(name)) {
+      return;
+    }
+    if (-1 !== stopped.indexOf(name)) {
+      return;
+    }
+    console.log('stopping: ' + name);
+    var callback = services[name].stop;
+    var promise = Promise.resolve(callback());
+    stopping.push(name);
+    return promise.then(function() {
+      console.log('stopped:  ' + name);
+      stopped.push(name);
+      return stop(dag.whereAllChildrenIn(stopped));
+    });
+  }));
+};
+
+start(dag.parentless()).then(function() {
+  // TODO assert that all are really running
+  console.log('running:  ALL');
+  return Promise.delay(1000);
+}).then(function() {
+  return stop(dag.childless());
+}).then(function() {
+  console.log('stopped:  ALL');
+});
